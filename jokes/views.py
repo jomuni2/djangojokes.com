693042cbee1c03b884acea8d106eb7aa0f3d02bd
jeongeunlee <jeongeunlee@isu.edu>
2024-09-10 11:55:36 -0600

    Created a DetailView and added get_absolute_url.

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Joke
 
 # Create your views here.
 class JokeListView(ListView):
+    model = Joke
+
+class JokeDetailView(DetailView):
     model = Joke
