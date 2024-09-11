6c6fff7d7e726e49a99c9167153064a7a59ca99d
jeongeunlee <jeongeunlee@isu.edu>
2024-09-11 16:01:19 -0600

    Created a DeleteView, so it can delete a joke&confirm in the website.

--- jokes/views.py
+++ jokes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.urls import reverse_lazy
+
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView, UpdateView)
 from .models import Joke
 
 # Create your views here.
 class JokeCreateView(CreateView):
     model = Joke
     fields = ['question', 'answer']
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
 
 class JokeListView(ListView):
     model = Joke
