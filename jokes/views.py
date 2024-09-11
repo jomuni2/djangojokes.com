3678dfab4fed71907f55bd6e5502c8d5ec71b635
jeongeunlee <jeongeunlee@isu.edu>
2024-09-11 15:18:20 -0600

    Created a CreateView and UpdateView, so now can create new jokes and update them in the website.

--- jokes/views.py
+++ jokes/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .models import Joke
 
 # Create your views here.
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
 class JokeListView(ListView):
     model = Joke
 
 class JokeDetailView(DetailView):
     model = Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
