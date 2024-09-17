012b79d787abc4a79bcb3e1ac57f2b52357a148b
jeongeunlee <jeongeunlee@isu.edu>
2024-09-17 11:28:05 -0600

    Added the Jokes table to Django Admin.

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,17 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
+
+from .models import Joke
 
 # Register your models here.
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return ('created', 'updated')
+        
+        return()
