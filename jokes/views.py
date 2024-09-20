1ba32fd5283e1e23f55ace00b17693b49baeada3
jeongeunlee <jeongeunlee@isu.edu>
2024-09-20 15:39:45 -0600

    Created a ModelForm.

--- jokes/views.py
+++ jokes/views.py
@@ -4,11 +4,12 @@ from django.urls import reverse_lazy
 
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView, UpdateView)
 from .models import Joke
+from .forms import JokeForm
 
 # Create your views here.
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 class JokeDeleteView(DeleteView):
     model = Joke
@@ -22,4 +23,4 @@ class JokeDetailView(DetailView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
