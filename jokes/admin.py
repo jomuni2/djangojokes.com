d8622e3de07897a3f99ed4f264f3750be67b4b03
jeongeunlee <jeongeunlee@isu.edu>
2024-09-20 16:47:57 -0600

    Added Joke Categories.

--- jokes/admin.py
+++ jokes/admin.py
@@ -2,9 +2,19 @@ from typing import Any
 from django.contrib import admin
 from django.http import HttpRequest
 
-from .models import Joke
+from .models import Category, Joke
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
     model = Joke
