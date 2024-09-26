a291ccde00e61a865e5a82b99279f4fb92cdec0d
jeongeunlee <jeongeunlee@isu.edu>
2024-09-26 16:46:32 -0600

    Restricted Joke Creating and Updating with Mixins; only a user who created the joke can modify it.

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.urls import reverse_lazy
 
@@ -7,7 +8,7 @@ from .models import Joke
 from .forms import JokeForm
 
 # Create your views here.
-class JokeCreateView(CreateView):
+class JokeCreateView(LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
 
@@ -15,9 +16,13 @@ class JokeCreateView(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class JokeDeleteView(DeleteView):
+class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
 class JokeListView(ListView):
     model = Joke
@@ -25,6 +30,10 @@ class JokeListView(ListView):
 class JokeDetailView(DetailView):
     model = Joke
 
-class JokeUpdateView(UpdateView):
+class JokeUpdateView(UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
