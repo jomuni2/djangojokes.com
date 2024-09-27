79b7d47320a876d7b344a9c036e7cd557e1e56d3
jeongeunlee <jeongeunlee@isu.edu>
2024-09-26 19:28:17 -0600

    Added Media Settings.

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -30,4 +32,4 @@ urlpatterns = [
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
