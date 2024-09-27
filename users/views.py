b6e7a28e03eae1a4d3ff2d50d9b5a02b3b904f65
jeongeunlee <jeongeunlee@isu.edu>
2024-09-26 18:42:31 -0600

    Shows a message "Update Successful" when there's a change in MyAccount using SuccessMessageMixin.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -13,9 +14,10 @@ class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy('my-account')
 
 # Create your views here.
-class MyAccountPageView(LoginRequiredMixin, UpdateView):
+class MyAccountPageView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
+    success_message = 'Update Successful'
     template_name = 'account/my_account.html'
 
     def get_object(self):
