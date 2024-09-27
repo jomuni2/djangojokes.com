e184c11fece42bf7838b40dfece290d71ebb15e7
jeongeunlee <jeongeunlee@isu.edu>
2024-09-26 18:00:41 -0600

    Added a built-in django messages to the AboutUsView.

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message')
+        messages.info(request, 'Info message')
+        messages.success(request, 'Success message')
+        messages.warning(request, 'Warning message')
+        messages.error(request, 'Error message')
+        return super().get(request, args, kwargs)
