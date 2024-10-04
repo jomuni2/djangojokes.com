f7fa8d081c871312bb8f2ffb0dfe52253425e02e
jeongeunlee <jeongeunlee@isu.edu>
2024-10-04 14:12:19 -0600

    Added properties to Joke model; numer of likes and dislikes.

--- jokes/models.py
+++ jokes/models.py
@@ -45,6 +45,19 @@ class Joke(models.Model):
         max_length=50, unique=True, null=False, editable=False
     )
 
+    @property
+    def num_votes(self):
+        return self.jokevotes.count()
+    
+    @property
+    def num_likes(self):
+        return self.jokevotes.filter(vote=1).count()
+    
+    @property
+    def num_dislikes(self):
+        return self.jokevotes.filter(vote=-1).count()
+    
+
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[str(self.slug)])
     
